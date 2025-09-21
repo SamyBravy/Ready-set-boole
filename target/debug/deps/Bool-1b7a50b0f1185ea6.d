8f559fc3f9d7073d1a1aec0dcbcef600564ea611
SamyBravy <samybravy@gmail.com>
2025-09-21 17:39:00 +0200

    gitignore

--- target/debug/deps/Bool-1b7a50b0f1185ea6.d
+++
@@ -1,9 +0,0 @@
-/home/sdell-er/Desktop/Progetti/Mastery/Matrix/Bool/target/debug/deps/libBool-1b7a50b0f1185ea6.rmeta: src/main.rs src/adder.rs src/multiplier.rs src/gray.rs src/boolean_evaluation.rs
-
-/home/sdell-er/Desktop/Progetti/Mastery/Matrix/Bool/target/debug/deps/Bool-1b7a50b0f1185ea6.d: src/main.rs src/adder.rs src/multiplier.rs src/gray.rs src/boolean_evaluation.rs
-
-src/main.rs:
-src/adder.rs:
-src/multiplier.rs:
-src/gray.rs:
-src/boolean_evaluation.rs:
--- target/debug/deps/Bool-a5f99e0c6cbae407.d
+++
@@ -1,9 +0,0 @@
-/home/lmicheli/Documents/Rust/Matrix/bool/target/debug/deps/Bool-a5f99e0c6cbae407: src/main.rs src/adder.rs src/boolean_evaluation.rs src/gray.rs src/multiplier.rs
-
-/home/lmicheli/Documents/Rust/Matrix/bool/target/debug/deps/Bool-a5f99e0c6cbae407.d: src/main.rs src/adder.rs src/boolean_evaluation.rs src/gray.rs src/multiplier.rs
-
-src/main.rs:
-src/adder.rs:
-src/boolean_evaluation.rs:
-src/gray.rs:
-src/multiplier.rs:
