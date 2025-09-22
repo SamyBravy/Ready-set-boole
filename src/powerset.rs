67901424f6a0618db5ec261f0460666218cd2bd2
SamyBravy <samybravy@gmail.com>
2025-09-23 01:00:01 +0200

    Implement initial powerset function in powerset.rs

---
+++ src/powerset.rs
@@ -0,0 +1,17 @@
+
+
+fn powerset(set: Vec<i32>) -> Vec<Vec<i32>>
+{
+	let mut powset = Vec::new();
+
+	for i in 0..(1 << set.len())
+	{
+		let mut new_set = Vec::new();
+
+		let mut j = i;
+		while j > 0
+		{
+			
+		}
+	}
+}
