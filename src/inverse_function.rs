4e75e08847b6a4ae2f2470e986a46a7d69eb3ad3
SamyBravy <samybravy@gmail.com>
2025-09-24 15:30:54 +0200

    FINE

--- src/inverse_function.rs
+++ src/inverse_function.rs
@@ -1,8 +1,8 @@
 pub fn reverse_map(n: f64) -> (u16, u16) {
-	if n < 0.0 || n > 1.0 {
-		println!("Input must be in the range [0.0, 1.0]");
-		return (0, 0);
-	}
+    if n < 0.0 || n > 1.0 {
+        println!("Input must be in the range [0.0, 1.0]");
+        return (0, 0);
+    }
 
     let scaled: u32 = (n * (u32::MAX as f64)) as u32;
     let mut x: u32 = 0;
